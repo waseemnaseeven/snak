1a09e23378052ebc6b5c9d41b0cf1c1c35d5cf08
lucienfer <lucien.fernandez02@gmail.com>
2025-02-12 16:50:55 +0000

    prettier norme

--- client/src/interfaces/fileInfo.ts
+++ client/src/interfaces/fileInfo.ts
@@ -1,10 +1,10 @@
 /**
-* Interface representing file information
-* @interface
-* @property {string} name - The name of the file
-* @property {number} size - The size of the file in bytes
-* @property {string} type - The MIME type of the file
-*/
+ * Interface representing file information
+ * @interface
+ * @property {string} name - The name of the file
+ * @property {number} size - The size of the file in bytes
+ * @property {string} type - The MIME type of the file
+ */
 export interface FileInfo {
   name: string;
   size: number;
