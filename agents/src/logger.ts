2de8403c9cc56b8a364387f43104ed513ea38157
Etienne Duclos <ledude2707@gmail.com>
2025-03-26 14:36:51 +0100

    fix logger for mcp

--- agents/src/logger.ts
+++ agents/src/logger.ts
@@ -30,17 +30,23 @@ const format = winston.format.combine(
     (info) => `${info.timestamp} ${info.level}: ${info.message}`
   )
 );
-
-const transports = [
-  new winston.transports.Console(),
-
-  new winston.transports.File({
-    filename: 'logs/error.log',
-    level: 'error',
-  }),
-
-  new winston.transports.File({ filename: 'logs/combined.log' }),
-];
+let transports;
+try {
+   transports = [
+    new winston.transports.Console(),
+  
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+    }),
+  
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ];
+} catch (error) {
+  transports = [
+    new winston.transports.Console(),
+  ];
+}
 
 const logger = winston.createLogger({
   level: level(),
