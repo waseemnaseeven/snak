bbd1f2be446a86402ee7f8bebff84f7e540e5a8f
antiyro <antiyro@gmail.com>
2025-05-08 13:45:15 +0100

    deleted cache on large files

--- packages/client/.next/server/next-font-manifest.js
+++
@@ -1,2 +0,0 @@
-self.__NEXT_FONT_MANIFEST =
-  '{"pages":{},"app":{"/Users/antiyro/Documents/Projet/Kasar/snak/packages/client/src/app/layout":["static/media/a34f9d1faa5f3315-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
