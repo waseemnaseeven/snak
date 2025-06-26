667fb8a075fb59040ae0b5ded9135949c54156a4
minhanhld <101562768+minhanhld@users.noreply.github.com>
2025-06-26 14:33:31 +0200

    fix(core) : updated prettier to fix CI (#177)

---
+++ plugins/braavos/src/tools/signature_tools.ts
@@ -0,0 +1,22 @@
+import { SignatureTool } from '@snakagent/core';
+import { accountDetailsSchema } from '../schemas/schema.js';
+import { CreateBraavosAccountSignature } from '../actions/createAccount.js';
+import { DeployBraavosAccountSignature } from '../actions/deployAccount.js';
+
+export const registerSignatureTools = (
+  StarknetToolRegistry: SignatureTool[]
+) => {
+  (StarknetToolRegistry.push({
+    name: 'create_braavos_account',
+    description:
+      'create braavos account return the privateKey/publicKey/contractAddress',
+    execute: CreateBraavosAccountSignature,
+  }),
+    StarknetToolRegistry.push({
+      name: 'deploy_braavos_account',
+      description:
+        'deploy braavos account return the privateKey/publicKey/contractAddress',
+      schema: accountDetailsSchema,
+      execute: DeployBraavosAccountSignature,
+    }));
+};
